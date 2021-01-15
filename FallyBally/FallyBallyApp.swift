034cbe77a3d391d2a93709fae39a4febb253a34e
Samuel McBroom <sam@sammcb.com>
2021-01-15 09:05:34 -0500

    Initial Commit

---
+++ FallyBally/FallyBallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FallyBallyApp.swift
+//  FallyBally
+//
+//  Created by Samuel McBroom on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct FallyBallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
