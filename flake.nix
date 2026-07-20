42a80cbabedc854f1801b483598d780545e82dbd
Samuel McBroom <sam@sammcb.com>
2026-07-20 07:12:29 -0700

    Update versions.

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
 	inputs = {
 		# Commit does not correspond to a tag.
 		# Updating to latest commit generally follows unstable branch.
-		nixpkgs.url = "github:NixOS/nixpkgs/030a23777772752542b0cba04ae37878997ebe50";
+		nixpkgs.url = "github:NixOS/nixpkgs/7c24fefe6c95bf92e591260d817bec87b44c885d";
 		# Commit does not correspond to a tag.
-		flake-parts.url = "github:hercules-ci/flake-parts/f7c1a2d347e4c52d5fb8d10cb4d94b5884e546fb";
+		flake-parts.url = "github:hercules-ci/flake-parts/8b9498dace8a531cef6972cadcd042591fbc49ad";
 		flake-checker = {
 			# Commit corresponds to tag v0.2.13.
 			url = "github:DeterminateSystems/flake-checker/ec67ebf5b7a666b3509006c3e2ce5e7dfbcaaf38";
